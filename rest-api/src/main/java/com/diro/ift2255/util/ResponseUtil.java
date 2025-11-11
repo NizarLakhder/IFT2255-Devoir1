fa681ed79b8fe7f7c2aa7792450fd3d7bda44ba4
tarekzerroug <tarekzerroug2@gmail.com>
2025-11-10 22:21:20 -0500

    Delete rest-api directory

--- rest-api/src/main/java/com/diro/ift2255/util/ResponseUtil.java
+++
@@ -1,9 +0,0 @@
-package com.diro.ift2255.util;
-
-import java.util.Map;
-
-public class ResponseUtil {
-    public static Map<String, String> formatError(String errorMessage) {
-        return Map.of("error", errorMessage);
-    }
-}
